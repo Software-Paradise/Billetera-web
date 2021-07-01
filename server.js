315128528df1fbdb115852dacf5929832b2f64a3
msobalvarro <mssrsobalvarro1997@gmail.com>
2021-07-01 10:12:21 -0600

    chance reset cache

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    // res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.send("Site under maintenance")
 })
 console.log('listen in', port)
 app.listen(port)
